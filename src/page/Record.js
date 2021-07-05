962b4b57279650304705f725e5bb0924f96a5480
banggu0321 <99bang@naver.com>
2021-07-05 17:25:43 +0900

    add record.js

--- src/page/Record.js
+++ src/page/Record.js
@@ -3,54 +3,10 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Record = () => {
-
-  const[playing, setPlaying] = useState(false);
-
-  const HEIGHT=500;
-  const WIDTH=500;
-
-  const startVideo =() =>{
-    setPlaying(true);
-    navigator.getUserMedia(
-			{
-				video: true,
-			},
-			(stream) => {
-				let video = document.getElementsByClassName('app__videoFeed')[0];
-				if (video) {
-					video.srcObject = stream;
-				}
-			},
-			(err) => console.error(err)
-		);
-  }
-
-  const stopVideo= () =>{
-    setPlaying(false);
-    let video = document.getElementsByClassName('app__videoFeed')[0];
-		video.srcObject.getTracks()[0].stop();
-  }
-
   return (
     <div className="Page">
       <header className="Page-header">
-        <div className="cam">
-          <video
-            height={HEIGHT}
-            width={WIDTH}
-            muted
-            autoPlay
-            className="app__videoFeed"
-          >
-          </video>
-        </div>
-        <div className="record">
-          {playing ? (
-            <button onClick={stopVideo}>Stop</button>
-          ) : (
-            <button onClick={startVideo}>Start</button>
-          )}
-        </div>
+
       </header>
     </div>
   );
